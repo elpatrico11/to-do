6dabde6365846b2b01b780395d4a76f58d2f5f91
elpatrico11 <elpatrico21@gmail.com>
2024-05-09 09:02:24 +0200

    added typescript

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { MDBFooter } from 'mdb-react-ui-kit';
 
-export default function Footer() {
+const Footer: FC = () => {
   return (
     <div className='footer'>
       <MDBFooter bgColor='dark' className='text-center text-lg-left'>
         <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
           &copy; {new Date().getFullYear()} Copyright:{' '}
           <a className='text-light' href='https://github.com/elpatrico11'>
-          ElPatrico11
+            ElPatrico11
           </a>
         </div>
       </MDBFooter>
     </div>
   );
-}
+};
 
+export default Footer;
