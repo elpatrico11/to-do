6266d9b2987856b512c90a5b4c5b9b411e5c7b3e
elpatrico11 <elpatrico21@gmail.com>
2024-05-23 11:44:02 +0200

    "Added a server, local API"

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,19 +1,18 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { MDBFooter } from 'mdb-react-ui-kit';
 
-const Footer: FC = () => {
+export default function Footer() {
   return (
     <div className='footer'>
       <MDBFooter bgColor='dark' className='text-center text-lg-left'>
         <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
           &copy; {new Date().getFullYear()} Copyright:{' '}
           <a className='text-light' href='https://github.com/elpatrico11'>
-            ElPatrico11
+          ElPatrico11
           </a>
         </div>
       </MDBFooter>
     </div>
   );
-};
+}
 
-export default Footer;
